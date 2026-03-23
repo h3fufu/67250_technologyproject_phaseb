c334ffc465a491a0ecca737b0d31b47ab96ff780
Helen Fu <h3fufu@gmail.com>
2026-03-23 00:15:42 -0400

    Increment 3 Done

---
+++ helenf-increment3/static/script.js
@@ -0,0 +1,16 @@
+var x = 5
+var y = 7
+var z = x + y
+console.log(z)
+
+var A = 'Hello '
+var B = 'world!'
+var C = A + B
+console.log(C)
+
+function sumnPrint(x1, x2) {
+    return x1 + x2
+}
+
+console.log(sumnPrint(x, y))
+console.log(sumnPrint(A, B))
